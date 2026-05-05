e45d02b0864a6cc59f3be267ba58870a424d937c
Stiven Ortiz Noreña <stivenestortiz@gmail.com>
2026-05-05 08:56:06 -0500

    Initial commit

---
+++ app/src/main/java/com/stiivenortiz/pruebatecnicaredeban/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.stiivenortiz.pruebatecnicaredeban
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.stiivenortiz.pruebatecnicaredeban.ui.theme.PruebaTecnicaRedebanTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PruebaTecnicaRedebanTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PruebaTecnicaRedebanTheme {
+        Greeting("Android")
+    }
+}
